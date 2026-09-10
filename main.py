337ad13632c2f993dd3407c5eeb738811551255a
NickJayJohnson <nickjakob@gmx.de>
2026-09-10 12:26:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -44,11 +44,11 @@ def shop_page(request: Request):
 def home(request: Request):
     return templates.TemplateResponse(request=request, name="index.html")
 
-@App.Komm("/admin/login", response_class=HTMLResponse)
+@app.get("/admin/login", response_class=HTMLResponse)
 def login_page(Anfrage: Anfrage):
     return templates.TemplateResponse(request=request, name="login.html", context={"error": None})
 
-@App.Posten("/admin/login", response_class=HTMLResponse)
+@app.post("/admin/login", response_class=HTMLResponse)
 def login_submit(request: Request, username: str = Form(...), password: str = Form(...)):
     is_user = secrets.compare_digest(username.encode("utf8"), ADMIN_USER.encode("utf8"))
     is_pass = secrets.compare_digest(password.encode("utf8"), ADMIN_PASSWORD.encode("utf8"))
